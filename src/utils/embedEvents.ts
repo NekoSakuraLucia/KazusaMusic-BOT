169372e41fe380b198a9a6cc897b64f8ab1054aa
NekoSakuraLucia <mokurolucia@gmail.com>
2025-05-20 15:45:42 +0700

    ปรับปรุง format ใหม่

--- src/utils/embedEvents.ts
+++ src/utils/embedEvents.ts
@@ -1,78 +1,144 @@
-import { EmbedBuilder } from "discord.js";
-import { InteractionEmbed } from "src/types";
+import { EmbedBuilder } from 'discord.js';
+import { InteractionEmbed } from 'src/types';
 
 // Embed การทำงานฟังชั่น
 
-export const PinkColor = '#F9A8D4'
+export const PinkColor = '#F9A8D4';
 
 export function JoinVoiceChannel(embedData: InteractionEmbed): EmbedBuilder {
-        const { interaction, client } = embedData;
+    const { interaction, client } = embedData;
 
-        return new EmbedBuilder()
-                .setAuthor({ name: interaction.user.displayName, iconURL: interaction.user.displayAvatarURL() ?? '' })
-                .setTitle('เกิดข้อผิดพลาด')
-                .setDescription('**กรุณาเข้าห้องเสียงก่อนค่ะ !**')
-                .setColor(PinkColor)
-                .setFooter({ text: client.user?.displayName as string, iconURL: client.user?.displayAvatarURL() ?? '' })
-                .setTimestamp();
+    return new EmbedBuilder()
+        .setAuthor({
+            name: interaction.user.displayName,
+            iconURL: interaction.user.displayAvatarURL() ?? '',
+        })
+        .setTitle('เกิดข้อผิดพลาด')
+        .setDescription('**กรุณาเข้าห้องเสียงก่อนค่ะ !**')
+        .setColor(PinkColor)
+        .setFooter({
+            text: client.user?.displayName as string,
+            iconURL: client.user?.displayAvatarURL() ?? '',
+        })
+        .setTimestamp();
 }
 
 export function SameRoom(embedData: InteractionEmbed): EmbedBuilder {
-        const { interaction, client } = embedData;
+    const { interaction, client } = embedData;
 
-        return new EmbedBuilder()
-                .setAuthor({ name: interaction.user.displayName, iconURL: interaction.user.displayAvatarURL() ?? '' })
-                .setTitle('เกิดข้อผิดพลาด')
-                .setDescription('**คุณต้องอยู่ในห้องเสียงเดียวกับหนูนะ !**')
-                .setColor(PinkColor)
-                .setFooter({ text: client.user?.displayName as string, iconURL: client.user?.displayAvatarURL() ?? '' })
-                .setTimestamp();
+    return new EmbedBuilder()
+        .setAuthor({
+            name: interaction.user.displayName,
+            iconURL: interaction.user.displayAvatarURL() ?? '',
+        })
+        .setTitle('เกิดข้อผิดพลาด')
+        .setDescription('**คุณต้องอยู่ในห้องเสียงเดียวกับหนูนะ !**')
+        .setColor(PinkColor)
+        .setFooter({
+            text: client.user?.displayName as string,
+            iconURL: client.user?.displayAvatarURL() ?? '',
+        })
+        .setTimestamp();
 }
 
 export function NotPlaying(embedData: InteractionEmbed): EmbedBuilder {
-        const { interaction, client } = embedData;
+    const { interaction, client } = embedData;
 
-        return new EmbedBuilder()
-                .setAuthor({ name: interaction.user.displayName, iconURL: interaction.user.displayAvatarURL() ?? '' })
-                .setTitle('เกิดข้อผิดพลาด')
-                .setDescription('**ไม่มีเพลงอะไรเล่นตอนนี้ !**')
-                .setColor(PinkColor)
-                .setFooter({ text: client.user?.displayName as string, iconURL: client.user?.displayAvatarURL() ?? '' })
-                .setTimestamp();
+    return new EmbedBuilder()
+        .setAuthor({
+            name: interaction.user.displayName,
+            iconURL: interaction.user.displayAvatarURL() ?? '',
+        })
+        .setTitle('เกิดข้อผิดพลาด')
+        .setDescription('**ไม่มีเพลงอะไรเล่นตอนนี้ !**')
+        .setColor(PinkColor)
+        .setFooter({
+            text: client.user?.displayName as string,
+            iconURL: client.user?.displayAvatarURL() ?? '',
+        })
+        .setTimestamp();
 }
 
 export function NotConnectVoice(embedData: InteractionEmbed): EmbedBuilder {
-        const { interaction, client } = embedData;
+    const { interaction, client } = embedData;
 
-        return new EmbedBuilder()
-                .setAuthor({ name: interaction.user.displayName, iconURL: interaction.user.displayAvatarURL() ?? '' })
-                .setTitle('เกิดข้อผิดพลาด')
-                .setDescription('**หนูไม่ได้เชื่อมต่อกับห้องเสียง !**')
-                .setColor(PinkColor)
-                .setFooter({ text: client.user?.displayName as string, iconURL: client.user?.displayAvatarURL() ?? '' })
-                .setTimestamp();
+    return new EmbedBuilder()
+        .setAuthor({
+            name: interaction.user.displayName,
+            iconURL: interaction.user.displayAvatarURL() ?? '',
+        })
+        .setTitle('เกิดข้อผิดพลาด')
+        .setDescription('**หนูไม่ได้เชื่อมต่อกับห้องเสียง !**')
+        .setColor(PinkColor)
+        .setFooter({
+            text: client.user?.displayName as string,
+            iconURL: client.user?.displayAvatarURL() ?? '',
+        })
+        .setTimestamp();
 }
 
 export function SearchError(embedData: InteractionEmbed): EmbedBuilder {
-        const { interaction, client } = embedData;
+    const { interaction, client } = embedData;
 
-        return new EmbedBuilder()
-                .setAuthor({ name: interaction.user.displayName, iconURL: interaction.user.displayAvatarURL() ?? '' })
-                .setTitle('เกิดข้อผิดพลาด')
-                .setDescription('**เกิดข้อผิดพลาดระหว่างการค้นหาเพลง !**')
-                .setColor(PinkColor)
-                .setFooter({ text: client.user?.displayName as string, iconURL: client.user?.displayAvatarURL() ?? '' })
-                .setTimestamp();
+    return new EmbedBuilder()
+        .setAuthor({
+            name: interaction.user.displayName,
+            iconURL: interaction.user.displayAvatarURL() ?? '',
+        })
+        .setTitle('เกิดข้อผิดพลาด')
+        .setDescription('**เกิดข้อผิดพลาดระหว่างการค้นหาเพลง !**')
+        .setColor(PinkColor)
+        .setFooter({
+            text: client.user?.displayName as string,
+            iconURL: client.user?.displayAvatarURL() ?? '',
+        })
+        .setTimestamp();
 }
 
-export function SlashError(embedData: InteractionEmbed): EmbedBuilder {
-        const { interaction, client } = embedData;
+// Interaction
 
-        return new EmbedBuilder()
-                .setAuthor({ name: interaction.user.displayName, iconURL: interaction.user.displayAvatarURL() ?? '' })
-                .setTitle('เกิดข้อผิดพลาด')
-                .setDescription('**เกิดข้อผิดพลาดขณะดำเนินการคำสั่งนี้!**')
-                .setColor(PinkColor)
-                .setFooter({ text: client.user?.displayName as string, iconURL: client.user?.displayAvatarURL() ?? '' })
-                .setTimestamp();
+export function SlashError(embedData: InteractionEmbed): EmbedBuilder {
+    const { interaction, client } = embedData;
+
+    return new EmbedBuilder()
+        .setAuthor({
+            name: interaction.user.displayName,
+            iconURL: interaction.user.displayAvatarURL() ?? '',
+        })
+        .setTitle('เกิดข้อผิดพลาด')
+        .setDescription('**เกิดข้อผิดพลาดขณะดำเนินการคำสั่งนี้!**')
+        .setColor(PinkColor)
+        .setFooter({
+            text: client.user?.displayName as string,
+            iconURL: client.user?.displayAvatarURL() ?? '',
+        })
+        .setTimestamp();
+}
+
+// isStringSelectMenu
+
+export function StringSelectMenuError(
+    embedData: InteractionEmbed,
+    errorDescription: string | null,
+    error: string | unknown
+): EmbedBuilder {
+    const { interaction, client } = embedData;
+
+    return new EmbedBuilder()
+        .setAuthor({
+            name: interaction.user.displayName,
+            iconURL: interaction.user.displayAvatarURL() ?? '',
+        })
+        .setTitle('เกิดข้อผิดพลาด')
+        .setDescription(
+            `**${
+                errorDescription ?? 'Unknown Error'
+            }**\n\`\`\`js\n${error}\n\`\`\``
+        )
+        .setColor(PinkColor)
+        .setFooter({
+            text: client.user?.displayName as string,
+            iconURL: client.user?.displayAvatarURL() ?? '',
+        })
+        .setTimestamp();
 }
