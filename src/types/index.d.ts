f6c2b568249a12c23f090f3a02343300324a3a47
NekoSakuraLucia <mokurolucia@gmail.com>
2025-05-27 21:11:59 +0700

    Update index.d.ts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,19 +1,29 @@
-import { ChatInputCommandInteraction, Client, Collection, CommandInteraction, Interaction, SlashCommandBuilder } from "discord.js";
-import { LavalinkManager } from "lavalink-client/dist/types";
+import {
+    ChatInputCommandInteraction,
+    Client,
+    Collection,
+    CommandInteraction,
+    Interaction,
+    SlashCommandBuilder,
+} from 'discord.js';
+import { LavalinkManager } from 'lavalink-client/dist/types';
 
-declare type InteractionExecute = (client: KazusaClient, interaction: ChatInputCommandInteraction<"cached">) => Promise<void> | void;
+declare type InteractionExecute = (
+    client: KazusaClient,
+    interaction: ChatInputCommandInteraction<'cached'>
+) => Promise<void> | void;
 
 export interface Command {
-        data: SlashCommandBuilder;
-        execute: InteractionExecute;
-};
+    data: SlashCommandBuilder;
+    execute: InteractionExecute;
+}
 
 export interface KazusaClient extends Client {
-        lavalink: LavalinkManager;
-        commands: Collection<string, any>
-};
+    lavalink: LavalinkManager;
+    commands: Collection<string, any>;
+}
 
 export interface InteractionEmbed {
-        interaction: Interaction;
-        client: Client
-};
+    interaction: Interaction;
+    client: Client;
+}
